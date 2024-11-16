fe22afbb3c5c9f630ce4076e77e4041b309ca654
PapiGelvez <s.gelvez@uniandes.edu.co>
2024-11-16 12:14:13 -0500

    Local storage strategy implemented (save login credentials)

--- app/src/main/java/com/papigelvez/a5palas12/profile/ProfileActivity.kt
+++ app/src/main/java/com/papigelvez/a5palas12/profile/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.papigelvez.a5palas12.profile
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -9,12 +10,16 @@ import androidx.core.view.WindowInsetsCompat
 import com.papigelvez.a5palas12.R
 import com.papigelvez.a5palas12.databinding.ActivityProfileBinding
 import com.papigelvez.a5palas12.home.HomeActivity
+import com.papigelvez.a5palas12.login.LoginActivity
 import com.papigelvez.a5palas12.map.MapActivity
 import com.papigelvez.a5palas12.search.SearchActivity
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfileBinding
+
+    private val sharedPreferences by lazy { getSharedPreferences("LoginCreds", Context.MODE_PRIVATE) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +47,14 @@ class ProfileActivity : AppCompatActivity() {
         }
         binding.linearColumnHome.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.linearRowLogout.setOnClickListener {
+            val editor = sharedPreferences.edit()
+            editor.remove("email")
+            editor.remove("password")
+            editor.apply()
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
